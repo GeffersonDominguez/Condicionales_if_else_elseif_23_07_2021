3f80a0adbc0ddfd087177d92389f08750e269b04
GeffersonDominguez <geffersondominguez93@gmail.com>
2021-08-08 17:34:22 -0500

    Condicionales IF ELSE 23-07--2021

---
+++ main.js
@@ -0,0 +1,41 @@
+
+// edad=prompt(`Ingrese edad`)
+
+// validacionEdad=(edad>=18)? `Es mayor de edad`:`Es menor de edad`
+// alert(validacionEdad)
+
+
+// let opciones = prompt(`
+// Ingrese una de las opciones para entrar a la puerta Inligente
+//     A. Ingresar la llave
+//     B. Huella dactilar
+//     C. Lectura de voz
+// `);
+
+// if( (opciones == 'A' || opciones == 'B') || opciones== 'C'){
+// //      (FALSE        ||     FALSE)      ||    TRUE
+// //                FALSE                   ||    TRUE
+// //                               TRUE
+//     console.log(`La puerta se esta abriendo BIENVENIDO usuario ingresantes la credencia correcta`);
+// }else{
+//     console.log(`La puerta esta cerrada acceso denegado :(`);
+
+// let nota;
+
+
+// nota= parseInt( prompt(`Ingrese su calificación`))
+
+
+// if (nota>=0 && nota<3) {
+
+//     alert(`Nota deficiente`)
+    
+// }else if(nota>=3 && nota<4) {
+//        alert(`Nota aceptable`)
+// }else if(nota>=4 && nota<5) {
+//         alert(`Nota sobresaliente`)
+// }else if(nota==5) {
+//         alert(`Nota excelente`)
+// } else {
+//     alert(`valor no permitido`)
+// }
